c6adf0a63ec281f8a89a1a735c51e900fad95c64
Eli Spicer <spicefe@gmail.com>
2023-07-31 16:40:51 -0400

    Banner CSS & Banner updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,9 +14,32 @@ export const Banner = () => {
 
     useEffect(() => {
         let ticker = setInterval(() => {
+            tick();
+        }, delta)
 
-        }, )
-    })
+        return () => {clearInterval(ticker)};
+    }, [text]);
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === "") {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
 
     return (
         <section className="banner" id="home">
@@ -24,7 +47,7 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio!</span>
-                        <h1>{"Hi I'm Eli!"}<span className="wrap">Full-Stack Software Engineer</span></h1>
+                        <h1>{"Hi I'm Eli! "}<span className="wrap">{text}</span></h1>
                         <p>
                             This is placeholder text. This text is meant to be a placeholder
                             until I put in text which is not a placeholder.
